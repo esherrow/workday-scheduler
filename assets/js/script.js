7e400c5975068d931a9f7af94f1af0399f2f6675
Yoga Cats <esherrow77@gmail.com>
2021-05-15 12:58:19 -0400

    save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var currentDay = $("#currentDay");
 var container = $(".container");
-currentDay.text(moment().format("LLLL"));
+currentDay.text(moment().format("MMMM Do YYYY"));
 
 var currentHour = moment().hour();
 
@@ -8,7 +8,8 @@ var hourArray=["8AM","9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", 
 var militaryHourArray=[8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (let i = 0; i < militaryHourArray.length; i++) {
-    var time = ""
+    
+  var time = ""
     if(currentHour === militaryHourArray[i]){
         time = "present";
     }
@@ -19,7 +20,7 @@ for (let i = 0; i < militaryHourArray.length; i++) {
 
     if(currentHour < militaryHourArray[i]){
         time = "future";
-    }
+    };
 
 
     container.append(`
@@ -29,13 +30,32 @@ for (let i = 0; i < militaryHourArray.length; i++) {
         </div>
         <div class="col-sm-10">
 
-          <textarea name="" id="" class="form-control w-100 ${time}"></textarea>
+          <textarea name="" id="textarea" class="tasks form-control w-100 ${time}"></textarea>
 
         </div>
         <div class="col-sm-1">
           <button class="saveBtn"><span class="oi oi-lock-locked mr-2"></span></button>
         </div>
       </div>
-    `)
-    
+    `);
 }
+
+var renderTasks = function(){
+  var tasks = localStorage.getItem('tasks');
+  //document.querySelectorAll("tasks").textContent = tasks;
+  //document.getElementsByClassName("tasks").innerHTML = tasks;
+}
+renderTasks();
+
+//var taskValue = document.querySelectorAll("tasks").value;
+var taskValue = document.getElementsByClassName("${time}").value;
+var saveButton = document.getElementsByClassName("saveBtn");  
+for (let i=0; i<saveButton.length; i++){
+  saveButton[i].addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("test")
+
+    localStorage.setItem("${time}", JSON.stringify(taskValue));
+    //localStorage.setItem("tasks", JSON.stringify(taskValue));
+    
+})};
